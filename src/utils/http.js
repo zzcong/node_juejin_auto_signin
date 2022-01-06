655154a8739f4ac2a05ff3b5e292a44a0975884b
zhangzhicong <zhangzhicong1@tal.com>
2022-01-06 16:07:17 +0800

    refactor: 添加userAgent

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,7 +2,10 @@ const axios = require('axios')
 const {log, getConfig} = require('./util')
 const {cookie} = getConfig()
 
-axios.defaults.headers['Cookie'] = cookie
+axios.defaults.headers = {
+  "Cookie": cookie,
+  "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
+}
 
 axios.interceptors.request.use(
   config => config,
