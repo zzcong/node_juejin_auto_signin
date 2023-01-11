c1824fef9df75d549ec0b0cedcb733a8a5948812
张志聪 <18310429590@163.com>
2023-01-11 15:00:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const CronJob = require('cron').CronJob
 const {log} = require('./utils/util')
 const juejin = require('./utils/juejin')
 
+let time = `0 0 9 * * *`
+
 let job = new CronJob('0 0 9 * * *', async () => {
   await juejin.checkIn()
   await juejin.draw()
